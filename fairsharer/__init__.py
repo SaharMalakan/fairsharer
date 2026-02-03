b3dc0b6a7f6431d13c75e4e34d7683e9ddbe5b11
Sahar <saharr.malakan@study.hs-duesseldorf.de>
2026-02-03 11:47:24 +0100

    Initial commit: fairsharer project

---
+++ fairsharer/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about fairsharer."""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Saharr Malakan"
+__email__ = "Saharr.Malakan@study.hs-duesseldorf.de"
+__version__ = "0.1.0"
