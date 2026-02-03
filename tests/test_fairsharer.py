abe82f9d5482ddd9e5898689d0859154124446cf
Sahar <saharr.malakan@study.hs-duesseldorf.de>
2026-02-03 12:26:31 +0100

    test_fairsharer

---
+++ tests/test_fairsharer.py
@@ -0,0 +1,13 @@
+"""Tests for the fairsharer.my_module module.
+"""
+from fairsharer import fair_sharer
+
+
+def test_fair_sharer():
+    """Test the examples given in the assignment."""
+
+    #1 iteration
+    assert fair_sharer([0, 1000, 800, 0], 1) == [100.0, 800.0, 900.0, 0.0]
+
+    #2 iterations
+    assert fair_sharer([0, 1000, 800, 0], 2) == [100.0, 890.0, 720.0, 90.0]
